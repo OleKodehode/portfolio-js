dcc9a6591d4fa0ddaf855c270661dc1bb8ed6f49
Ole Kodehode <ole.kodehode@gmail.com>
2025-10-15 14:22:34 +0200

    Small Change

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,5 +1,6 @@
 export default function ProjectCard({ projectDetails }) {
-  const { title, imgSrc } = projectDetails;
+  console.log(projectDetails);
+  const { title } = projectDetails;
   return (
     <article className="card gap-2">
       <img src="" alt="" width="150" height="150" className="rounded-xl" />
